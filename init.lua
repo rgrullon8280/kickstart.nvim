986c806ee1ec45cca6ba89d9a52aaba868db131d
Ricky Grullon <rgrullon8280@gmail.com>
2026-02-07 21:55:18 -0500

    not sure

--- init.lua
+++ init.lua
@@ -194,6 +194,10 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+vim.keymap.set('n', '<leader>sv', '<C-w>v', { desc = 'Create a vertical split' })
+vim.keymap.set('n', '<leader>sh', '<C-w>s', { desc = 'Create a horizontal split' })
+vim.keymap.set('n', '<leader>sx', ':close<CR>', { desc = 'Closes the current split' })
+
 -- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
 -- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
 -- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
@@ -829,7 +833,6 @@ require('lazy').setup({
         -- <c-k>: Toggle signature help
         --
         -- See :h blink-cmp-config-keymap for defining your own keymap
-        preset = 'super-tab',
         ['<c-l'] = { 'select_prev', 'fallback' },
         ['<c-k>'] = { 'select_next', 'fallback' },
 
@@ -972,7 +975,7 @@ require('lazy').setup({
   -- require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 

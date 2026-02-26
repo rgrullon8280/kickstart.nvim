e9f7e7eb759c63396ae1d0a2623e84812f9566b3
Ricky Grullon <rgrullon@figma.com>
2026-02-26 10:44:07 -0500

    change keymap search

--- init.lua
+++ init.lua
@@ -471,7 +471,7 @@ require('lazy').setup({
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-      vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
+      vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = '[F]ind [K]eymaps' })
       vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<leader>fA', function()
         builtin.find_files { hidden = true, no_ignore = true }
